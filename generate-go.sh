37f2dfcff480f6b3f6825d3f825180940d4628c7
Victor Barua <victor.barua@datadoghq.com>
2025-04-16 10:51:26 -0700

    push branch and tag in ./generate-go.sh

--- generate-go.sh
+++ generate-go.sh
@@ -2,32 +2,36 @@
 
 # Ensure exactly one argument is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <tag>"
+    echo "Usage: $0 <version>"
     exit 1
 fi
 
-TAG="$1"
+VERSION="$1"
 SPEC_REPO_URL="https://github.com/substrait-io/substrait.git"
 
 # Use git ls-remote to check if the specific tag exists
-echo "Checking if tag '$TAG' exists in $SPEC_REPO_URL"
-if git ls-remote --tags "$SPEC_REPO_URL" "refs/tags/$TAG" | grep -q .; then
-    echo "✅ Tag '$TAG' exists."
+echo "Checking if tag '$VERSION' exists in $SPEC_REPO_URL"
+if git ls-remote --tags "$SPEC_REPO_URL" "refs/tags/$VERSION" | grep -q .; then
+    echo "✅ Tag '$VERSION' exists."
 else
-    echo "❌ Tag '$TAG' does NOT exist."
+    echo "❌ Tag '$VERSION' does NOT exist."
     exit 2
 fi
 
-BRANCH_NAME="releases/go/$TAG"
+BRANCH_NAME="releases/go/$VERSION"
 echo "🔨 Creating new branch: $BRANCH_NAME"
 git checkout -b "$BRANCH_NAME"
 
-TARGET="https://github.com/substrait-io/substrait.git#tag=$TAG"
+TARGET="https://github.com/substrait-io/substrait.git#tag=$VERSION"
 echo "🔧 Executing Protobuf code generation for $TARGET"
 buf generate "$TARGET"
 
 echo "Committing generated code"
 
 git add go/substraitpb
-git commit --allow-empty -m "generated go/substraitpb for $TAG"
-git tag "go/$TAG" -m "Generated Go code for spec version $TAG"
+git commit -m "generated go/substraitpb for $VERSION"
+git push --set-upstream origin "$BRANCH_NAME"
+
+VERSION_TAG="go/$VERSION"
+git tag "$VERSION_TAG" -m "Generated Go code for spec version $VERSION"
+git push origin "$VERSION_TAG"
